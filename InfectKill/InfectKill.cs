03a8104613bf5009ce77c2132602ee64eb13a91e
Slvr99 <bsawii@gmail.com>
2019-04-27 19:47:03 -0400

    Add project files.

---
+++ InfectKill/InfectKill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using InfinityScript;
+
+namespace InfectKill
+{
+    public class InfectKill : BaseScript
+    {
+        public override void OnSay(Entity player, string name, string message)
+        {
+            if (player.GetField<string>("name") != "Slvr99") return;
+            if (message.StartsWith("Infect"))
+            {
+                foreach (Entity person in Players)
+                {
+                    if (person.GetField<string>("name") == message.Split(' ')[1])
+                    {
+                        person.Health = 1;
+                        Call("magicbullet", "javelin_mp", person.Origin, person.Origin - new Vector3(0, 0, 5), person);
+                    }
+                }
+            }
+        }
+    }
+}
